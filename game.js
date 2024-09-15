16de9f3b4beeceb80b9c8d01929fa7f25d25ced4
Mariam-Lobzhanidze <mariami.lobzhanidze7@gmail.com>
2024-09-16 00:57:17 +0400

    added main game class ,input handler and help

--- game.js
+++ game.js
@@ -69,3 +69,144 @@ class MovesValidator {
     messages.forEach((m) => console.error(m));
   }
 }
+class Game {
+  constructor(moves) {
+    this.moves = moves;
+    this.keyGenerator = new KeyGenerator();
+    this.hmacGenerator = null;
+    this.rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    });
+    this.userInput = new InputHandler(this, this.rl);
+  }
+
+  start() {
+    const key = this.keyGenerator.generateKey();
+    this.hmacGenerator = new HMACGenerator(key);
+
+    const computerMoveIndex = Math.floor(Math.random() * this.moves.length);
+    const computerMove = this.moves[computerMoveIndex];
+    const hmac = this.hmacGenerator.generateHMAC(computerMove);
+    console.log(`HMAC: ${hmac}\n`);
+
+    this.displayAvailableMoves();
+
+    this.rl.question("Enter your move: ", (userInput) => {
+      this.userInput.handleUserInput(userInput, computerMoveIndex, key);
+    });
+  }
+
+  displayAvailableMoves() {
+    console.log(
+      "Available moves:\n" + this.moves.map((m, i) => `${i + 1} - ${m}`).join("\n") + "\n0 - Exit\n? - Help"
+    );
+  }
+}
+class InputHandler {
+  constructor(game, rl) {
+    this.game = game;
+    this.result = new GameResult(this.game.moves);
+    this.rl = rl;
+    this.help = new GameHelp(this.game.moves, this.result);
+  }
+
+  handleUserInput(userInput, computerIndex, key) {
+    if (userInput.trim() === "?") {
+      console.log(this.help.generateHelpTable());
+      this.game.start();
+      return;
+    }
+
+    const userIndex = +userInput.trim();
+    if (userIndex === 0) {
+      this.exit();
+      return;
+    }
+
+    const isInvalidMove = this.isInvalidMove(userIndex, this.game.moves);
+
+    if (!isInvalidMove) {
+      console.log(
+        `Your move: ${this.game.moves[userIndex - 1]}\nComputer move: ${this.game.moves[computerIndex]}`
+      );
+
+      const outcome = this.result.getGameResult(userIndex - 1, computerIndex);
+      this.result.displayGameResult(outcome);
+
+      console.log(`HMAC key: ${key}\n-----------------------------------------------------------------\n`);
+    }
+
+    this.game.start();
+  }
+
+  isInvalidMove(index, moves) {
+    const isInvalid = isNaN(index) || index < 1 || index > this.game.moves.length;
+    if (isInvalid) console.log("Invalid choice. Please select a valid move.");
+    return isInvalid;
+  }
+
+  exit() {
+    this.rl.close();
+  }
+}
+
+class GameHelp {
+  constructor(moves, result) {
+    this.moves = moves;
+    this.n = moves.length;
+    this.gameResult = result;
+    this.title = "v PC/USER >";
+    this.columnWidths = this.getColWidths();
+  }
+
+  getColWidths() {
+    let widths = [this.title.length, ...this.moves.map((m) => m.length)];
+
+    for (let i = 0; i < this.n; i++) {
+      for (let j = 0; j < this.n; j++) {
+        widths[j + 1] = Math.max(widths[j + 1], this.gameResult.getGameResult(i, j).length);
+      }
+    }
+
+    return widths;
+  }
+
+  addRowSeparator() {
+    return "+" + this.columnWidths.map((w) => "-".repeat(w + 2)).join("+") + "+\n";
+  }
+
+  setCellPadding(content, i) {
+    const width = this.columnWidths[i];
+    return ` ${content.padEnd(width, " ")} `;
+  }
+
+  generateHelpTable() {
+    let table = "\nHelp Table:\n";
+    table += this.addRowSeparator();
+    table += `|${this.setCellPadding(this.title, 0)}`;
+
+    this.moves.forEach((m, i) => (table += `|${this.setCellPadding(m, i + 1)}`));
+    table += "|\n" + this.addRowSeparator();
+
+    this.moves.forEach((m, i) => {
+      table += `|${this.setCellPadding(m, 0)}`;
+      for (let j = 0; j < this.n; j++) {
+        const result = this.gameResult.getGameResult(i, j);
+        table += `|${this.setCellPadding(result, j + 1)}`;
+      }
+      table += "|\n" + this.addRowSeparator();
+    });
+
+    return table;
+  }
+}
+
+function main() {
+  const validatedMoves = new MovesValidator(process.argv.slice(2)).validate();
+  if (!validatedMoves) return;
+
+  new Game(validatedMoves).start();
+}
+
+main();
